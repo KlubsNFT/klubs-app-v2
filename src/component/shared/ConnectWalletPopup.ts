2ca0602cfe472144d848173ce8ae8a05cfca1e76
dilrong <dilrong@dilrong.com>
2022-06-27 21:15:43 +0900

    feat: connectwalletpopup

--- src/component/shared/ConnectWalletPopup.ts
+++ src/component/shared/ConnectWalletPopup.ts
@@ -9,28 +9,37 @@ export default class ConnectWalletPopup extends Popup {
         super(".popup-background");
         this.append(
             this.content = el(".connect-wallet-popup",
-                el("h2", msg("CONNECT_WALLET_TITLE")),
-                el("p", msg("CONNECT_WALLET_DESC")),
-                el(".button-container",
-                    el("a.connect-kaikas-button",
-                        el("img", { src: "/images/shared/logo/kaikas.svg" }),
-                        msg("CONNECT_WALLET_BUTTON1"),
-                        { href: "https://chrome.google.com/webstore/detail/kaikas/jblndlipeogpafnldhgmapagcccfchpi" },
+                el(".left-container",
+                    el("p", "IMAGE"),
+                ),
+                el(".right-container",
+                    el(".cancel-container", { click: () => this.delete(), },
+                        el("img", { src: "/images/icn/close.svg", alt: "close" }),
                     ),
-                    el("a.connect-klip-button",
-                        el("img", { src: "/images/shared/logo/klip.svg" }),
-                        msg("CONNECT_WALLET_BUTTON2"),
-                        {
+                    el("header",
+                        el("h6", "Connect Wallet"),
+                        el("p", "KLUBS는 블록체인지갑과의 연결이 필요합니다. 카이카스 혹은 카카오톡 클립에서 연결해주시기 바랍니다."),
+                    ),
+                    el("hr"),
+                    el(".button-container",
+                        el("a.kaikas-button", { href: "https://chrome.google.com/webstore/detail/kaikas/jblndlipeogpafnldhgmapagcccfchpi" },
+                            el("img", { src: "/images/icn/kaikas.svg", alt: "kaikas" }),
+                            el("p", "카이카스"),
+                        ),
+                        el("a.klip-button", {
                             click: async () => {
                                 await Klip.connect();
                                 callback();
                                 this.delete();
                             },
                         },
+                            el("img", { src: "/images/icn/klip.svg", alt: "klip" }),
+                            el("p", "카카오클립"),
+                        ),
+                        el("a.cancel-button",
+                            el("p", "연결하지않고 둘러보기"),
+                        ),
                     ),
-                    el("a.button.cancel-button", msg("CONNECT_WALLET_BUTTON3"), {
-                        click: () => this.delete(),
-                    }),
                 ),
             ),
         );
